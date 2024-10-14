53188aa702b81a4d5eb4ca1403a97a0f7e12ac84
Oscar941 <128169229+Oscar941@users.noreply.github.com>
2024-10-14 20:42:51 +0300

    Create file models.py

---
+++ models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    notes = db.relationship('Note', backref='author', lazy=True)
+    notebooks = db.relationship('Notebook', backref='owner', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Note(db.Model):
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    notebook_id = db.Column(db.Integer, db.ForeignKey('notebooks.id'), nullable=True)
+
+    def __repr__(self):
+        return '<Note %r>' % self.title
+
+class Notebook(db.Model):
+    __tablename__ = 'notebooks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    notes = db.relationship('Note', backref='notebook', lazy=True)
+
+    def __repr__(self):
+        return '<Notebook %r>' % self.name
+
+def initialize_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
